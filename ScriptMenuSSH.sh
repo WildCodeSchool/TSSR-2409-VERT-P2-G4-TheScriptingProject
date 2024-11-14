ad7d3bc10dc1a5a52cac6c293ca7185168391f6d
Souhail9397 <souhail.aitouarraou@gmail.com>
2024-11-14 11:58:33 +0100

    Update ScriptMenuSSH.sh
    ajout while true; do (ligne 106)
    ajout done (ligne 120)

--- ScriptMenuSSH.sh
+++ ScriptMenuSSH.sh
@@ -103,6 +103,7 @@ echo "L'utilisateur $nom_utilisateur n'existe pas"
 fi
 }
 
+while true; do
 echo -e "Merci de choisir votre action en tapant :\n1 - Créer un compte utilisateur\n2 - Changer le mot de passe d'un utilisateur\n3 - Supprimer un compte utilisateur\n4 - Désactiver un groupe utilisateur\n5 - Ajouter à un groupe d'administrateur\n6 - Ajouter à un groupe local\n7 pour Sortie d'un groupe local"
 read -p "Choix:" action 
 
@@ -116,6 +117,7 @@ read -p "Choix:" action
                  7) removelocalgroup ;;
                  *) echo "Option invalide.";;
 	esac
+done
 fi
 
 
