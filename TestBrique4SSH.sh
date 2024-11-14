b6cce5820d541538db14003f21c539d971cc3b4d
Souhail9397 <souhail.aitouarraou@gmail.com>
2024-11-14 10:56:22 +0100

    Update TestBrique4SSH.sh

--- TestBrique4SSH.sh
+++ TestBrique4SSH.sh
@@ -1,1 +1,111 @@
+if [ "$choix" -eq 4 ] 
+then
 
+# 1 Version de l'OS
+function versionOS()
+{
+echo "La version des l'OS est:" 
+uname -r
+}
+
+# 2 Nombre de disques
+function ListDisk()
+{
+lsblk -d -o NAME,SIZE,TYPE
+}
+
+# 3 Fonction pour les partitions (nombre, nom, FS, taille) par disque
+function ListPartition()
+{
+lsblk -o NAME,FSSIZE,FSTYPE,LABEL
+}
+
+# 4 Fonction pour espace disque restant par partition/volume
+function FreeSpace()
+{
+lsblk -o NAME,FSAVAIL
+}
+
+# 5 Fonction pour le nom et espace disque d'un dossier (nom de dossier demandé)
+function FolderInfos()
+{
+while true
+do
+	read -p 'Emplacement absolu du dossier, "exit" pour sortir : ' FolderPlacement
+	if  [ -d "$FolderPlacement" ]
+	then
+    	while true
+    	do
+        	du -sh $FolderPlacement | cut -f1 && basename $FolderPlacement
+        	break
+    	done
+	elif [ $FolderPlacement = "exit" ]
+	then
+    	break
+	else
+    	echo -e "\033[31mCe dossier n'existe pas ou le champ est vide !\033[0m"
+	fi
+done
+}
+
+# 6 Fonction pour la liste des lecteurs montés (disque, CD, etc...)
+function MountedDrive()
+{
+df -h | grep "^/dev/"
+}
+
+# 7 Fonction pour le nombre d'interfaces
+function interfacesnumber()
+{
+echo "nombre d'interfaces : $(ip link show | grep -c '^[0-9]')"
+}
+
+# 8 Fonction pour l’adresse IP de chaque interface
+function interfaceip()
+{
+ip -o -4 addr show | while read -r line; do
+# Récupérer le nom de l'interface (colonne 2) et l'adresse IP (colonne 4)
+iface=$(echo "$line" | awk '{print $2}')
+ip_addr=$(echo "$line" | awk '{print $4}')
+# Afficher le nom de l'interface et son adresse IP
+echo "$iface : $ip_addr"
+done
+}
+
+# 9 Fonction pour l’adresse Mac
+function macaddr()
+{
+echo “L’adresse MAC de cet ordinateur est : “
+ip link show $interface | awk '/ether/ {print $2}'
+}
+
+# 10 Fonction pour la liste des ports ouverts
+function ports_ouverts()
+{
+sudo netstat –tulnp
+}
+
+# 11 Fonction pour le statut du pare-feu
+function status_firewall()
+{
+systemctl status firewall
+}
+
+echo -e "Veuillez choisir l'information désirée en tapant :\n1 - Version de l'OS\n2 - Nombre de disques\n3 - Partitions (nombre, nom, FS, taille) par disque\n4 - Espace disque restant par partition/volume\n5 - Nom et espace disque d'un dossier\n6 - Liste des lecteurs montés\n7 - Nombre d'interfaces\n8 - Adresses IP de chaque interface\n9 - Adresse MAC\n10 - Liste des ports ouverts\n11 - Statut du pare-feu"
+read -p "Choix:" info
+
+case "$info" in
+               1) versionOS ;;
+               2) ListDisk  ;;
+               3) ListPartition  ;;
+               4) FreeSpace  ;;
+               5) FolderInfos ;;
+               6) MountedDrive  ;;
+               7) interfacesnumber  ;;
+               8) interfaceip  ;;
+               9) macaddr  ;;
+               10) ports_ouverts ;;  
+               11) status_firewall ;;
+esac
+EOF
+fi
